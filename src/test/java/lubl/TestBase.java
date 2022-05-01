4e19e4793f4e52418ad045f616449575c1dd9929
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-05-01 19:19:41 +0300

    login selenoid

--- src/test/java/lubl/TestBase.java
+++ src/test/java/lubl/TestBase.java
@@ -2,20 +2,28 @@ package lubl;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
 
     @BeforeAll
     static void setUp() {
+        String login = config.login();
+        String password = config.password();
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://"+login+":"+password+"@"+"selenoid.autotests.cloud/wd/hub";
+
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
